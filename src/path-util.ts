f1cbcce0269758561bf22f655907569ee8abb0e8
Anthony Rey <gnuk@mailoo.org>
2022-10-10 16:32:23 +0200

    Resolves paths using posix separator

--- src/path-util.ts
+++ src/path-util.ts
@@ -1,6 +1,13 @@
 import path from 'path';
 
+export const toPosixPath = (currentPath: string): string => currentPath.split(path.win32.sep).join(path.posix.sep);
+
 export const basePathOf = (basedir: string) => (source: string): string => {
   const relative = path.relative(path.dirname(source), basedir);
-  return relative.length === 0 ? '' : `${relative}/`;
+
+  if(relative.length === 0) {
+    return '';
+  }
+
+  return `${toPosixPath(relative)}${path.posix.sep}`;
 };
