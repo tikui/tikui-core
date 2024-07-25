a364a7f9537b2cce2d3ada427d094afbf17b385c
Anthony Rey <gnuk@mailoo.org>
2024-07-25 11:25:16 +0200

    Remove montserrat font from libs

--- src/lib-resources.ts
+++ src/lib-resources.ts
@@ -25,20 +25,6 @@ export const libResources: LibResources = {
       from: 'themes/prism-okaidia.css',
       to: 'prism-okaidia.css',
     },
-  ],
-  '@fontsource/montserrat': [
-    {
-      from: 'files',
-      to: 'files'
-    },
-    {
-      from: 'index.css',
-      to: 'index.css'
-    },
-    {
-      from: '700.css',
-      to: '700.css'
-    },
   ]
 };
 
