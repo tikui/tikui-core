cf0c717aae4ae895bbe84c97e30e869f7df46b87
Anthony Rey <gnuk@mailoo.org>
2024-07-26 14:54:30 +0200

    Use @tikui/reload instead of reload

--- src/express.ts
+++ src/express.ts
@@ -10,7 +10,7 @@ import { onDocResources, sassRender } from './doc-resources';
 import { onExposedResources } from './exposed-resources';
 import { renderPugFile } from './pug-util';
 
-const reload = require('reload');
+const reload = require('@tikui/reload');
 
 const app = express();
 
